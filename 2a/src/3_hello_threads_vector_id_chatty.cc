dcae354d9ab1e442d208c3a96ab297b3781b8f4b
Alastair F. Donaldson <alastair.donaldson@imperial.ac.uk>
2023-10-10 17:45:51 +0100

    Threads telling us about their favourite foods.

--- 2a/src/3_hello_threads_vector_id_chatty.cc
+++ 2a/src/3_hello_threads_vector_id_chatty.cc
@@ -1,1 +1,29 @@
-int main() { return 0; }
+#include <thread>
+#include <iostream>
+#include <vector>
+
+static void ThreadBody(size_t nice_id, std::vector<std::string>& favourite_food) {
+  std::cout << "Hello! I am " << std::this_thread::get_id()
+            << ", but you can call me " << nice_id
+            << ", and my favourite food is "
+            << favourite_food[nice_id]
+            << std::endl;
+}
+
+int main() {
+
+  std::vector<std::string> foods = {
+      "Apple", "Spotted dick", "Pudding", "Haggis", "Quorn", "Pizza",
+      "Custard cream", "Katsu curry"
+  };
+
+  std::vector<std::thread> threads;
+  for (size_t i = 0; i < 8; i++) {
+    threads.emplace_back(ThreadBody, i, std::ref(foods));
+  }
+
+  for (auto& t : threads) {
+    t.join();
+  }
+
+}
