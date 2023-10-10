1bd802eca6858854667158f4052bfe9b8ad346ed
Alastair F. Donaldson <alastair.donaldson@imperial.ac.uk>
2023-10-10 17:45:03 +0100

    Launching many threads and joining them, using a vector.

--- 2a/src/2_hello_threads_vector.cc
+++ 2a/src/2_hello_threads_vector.cc
@@ -1,1 +1,20 @@
-int main() { return 0; }
+#include <thread>
+#include <iostream>
+#include <vector>
+
+static void ThreadBody() {
+  // As you will see from the output, the built-in thread ID is not very useful!
+  std::cout << "Hello! I am " << std::this_thread::get_id() << std::endl;
+}
+
+int main() {
+  std::vector<std::thread> threads;
+  for (size_t i = 0; i < 8; i++) {
+    threads.emplace_back(ThreadBody);
+  }
+
+  for (auto& t : threads) {
+    t.join();
+  }
+
+}
