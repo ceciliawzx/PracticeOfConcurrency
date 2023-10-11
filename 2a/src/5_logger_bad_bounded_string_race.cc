da311a1288ca93a8acc4434b14125c09219ac6b6
Cecilia <ceciliawzx@qq.com>
2023-10-11 16:06:10 +0100

    Lecture1

--- 2a/src/5_logger_bad_bounded_string_race.cc
+++ 2a/src/5_logger_bad_bounded_string_race.cc
@@ -5,30 +5,30 @@
 #include <vector>
 
 class Logger {
- public:
-  Logger(std::mutex& mutex, size_t max_length)
-      : mutex_(mutex), max_length_(max_length) {}
+public:
+    Logger(std::mutex &mutex, size_t max_length)
+        : mutex_(mutex), max_length_(max_length) {}
 
-  void LogMessage(const std::string& message) {
-    // Uh oh, this leads to a data race! Why?
-    if (log_.size() + message.size() > max_length_) {
-      return;
+    void LogMessage(const std::string &message) {
+      // Uh oh, this leads to a data race! Why?
+      if (log_.size() + message.size() > max_length_) {
+        return;
+      }
+      mutex_.lock();
+      log_ += message;
+      mutex_.unlock();
     }
-    mutex_.lock();
-    log_ += message;
-    mutex_.unlock();
-  }
 
-  [[nodiscard]] const std::string& GetLog() const { return log_; }
+    [[nodiscard]] const std::string &GetLog() const { return log_; }
 
- private:
-  std::string log_;
-  std::mutex& mutex_;
-  const size_t max_length_;
+private:
+    std::string log_;
+    std::mutex &mutex_;
+    const size_t max_length_;
 };
 
-static void ThreadBody(size_t nice_id, std::vector<std::string>& favourite_food,
-                       Logger& logger) {
+static void ThreadBody(size_t nice_id, std::vector<std::string> &favourite_food,
+                       Logger &logger) {
   std::stringstream stream;
   stream << "Hello! I am " << std::this_thread::get_id()
          << ", but you can call me " << nice_id << ", and my favourite food is "
@@ -37,24 +37,26 @@ static void ThreadBody(size_t nice_id, std::vector<std::string>& favourite_food,
 }
 
 int main() {
-  std::vector<std::string> foods = {"Apple",         "Spotted dick", "Pudding",
-                                    "Haggis",        "Quorn",        "Pizza",
+  std::vector<std::string> foods = {"Apple", "Spotted dick", "Pudding",
+                                    "Haggis", "Quorn", "Pizza",
                                     "Custard cream", "Katsu curry"};
 
-  std::mutex the_mutex;
-  Logger the_logger(the_mutex, 100);
-  std::vector<std::thread> threads;
-  for (size_t i = 0; i < 8; i++) {
-    threads.emplace_back(ThreadBody, i, std::ref(foods), std::ref(the_logger));
-  }
+  for (int j = 0; j < 100; j++) {
+    std::mutex the_mutex;
+    Logger the_logger(the_mutex, 100);
+    std::vector<std::thread> threads;
+    for (size_t i = 0; i < 8; i++) {
+      threads.emplace_back(ThreadBody, i, std::ref(foods), std::ref(the_logger));
+    }
 
-  for (auto& t : threads) {
-    t.join();
-  }
+    for (auto &t: threads) {
+      t.join();
+    }
 
-  std::string log_contents = the_logger.GetLog();
-  std::cout << log_contents << std::endl;
-  // Run this enough times, and you will see that the log contents sometimes
-  // ends up longer than the maximum value of 100 that we wanted to allow.
-  std::cout << log_contents.size() << std::endl;
+    std::string log_contents = the_logger.GetLog();
+    std::cout << log_contents << std::endl;
+    // Run this enough times, and you will see that the log contents sometimes
+    // ends up longer than the maximum value of 100 that we wanted to allow.
+    std::cout << log_contents.size() << std::endl;
+  }
 }
